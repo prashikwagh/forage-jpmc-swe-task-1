d5c9fd22a600799484516d00a388325fd64b76e5
prashikwagh <prashik.wagh22@vit.edu>
2024-06-27 01:26:31 +0530

    Add .idea/ directory

--- client3.py
+++ client3.py
@@ -30,30 +30,34 @@ N = 500
 
 
 def getDataPoint(quote):
-    """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
+    """ Produce all of the needed values to generate a datapoint """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
+
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return None
+    return price_a / price_b
 
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
-    for _ in iter(range(N)):
-        quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+    for _ in range(N):
+        quotes = json.loads(urllib.request.urlopen(QUERY).read())
 
-        """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            print(f"Quoted {stock} at (bid:{bid_price}, ask:{ask_price}), price: {price}")
 
-        print("Ratio %s" % getRatio(price, price))
+        price_a = getDataPoint(quotes[0])[3]
+        price_b = getDataPoint(quotes[1])[3]
+        ratio = getRatio(price_a, price_b)
+        print(f"Ratio: {ratio}")
+
